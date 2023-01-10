f939f8660c185eac181e955d5ebd571e90e7cc4a
Matheexx <89420055+Matheexx@users.noreply.github.com>
2023-01-10 14:04:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,31 +6,34 @@ const result = document.getElementById("result");
 input_button.addEventListener("click", () => {
     result.innerHTML = "";
     if (input_number.value.length == 0) {
-        input_number.value = "";
         alert("[ERROR] Type a number!");
     }
     else if (Number(input_number.value) < 0) {
-        input_number.value = "";
         alert("[ERROR] Type positive number!")
+    }
+    else if (Number(input_number.value) > 170) {
+        alert("[ERROR] Large number, type again!")
     }
     else {
         let number = Number(input_number.value);
-        if (number == 0 || number == 1) {
-            result.innerHTML = `<p>The factorial of ${number} is 1</p>`;
-            result.innerHTML += `<p>${number}! = 1</p>`
-        }
-        else {
-            function factorial(old_number) {
-                for (let i = old_number - 1; i >= 1; i--) {
-                    old_number = old_number * i;
-                }
-                return old_number;
-            }
 
-            let newnumber = factorial(number);
+        let newnumber = factorial(number);
 
-            result.innerHTML = `<p>The factorial of ${number} is ${newnumber}</p>`;
-            result.innerHTML += `<p>${number}! = ${newnumber}</p>`;
-        }
+        result.innerHTML = `<p>The factorial of ${number} is ${newnumber}</p>`;
+        result.innerHTML += `<p>${number}! = ${newnumber}</p>`;
     }
+    input_number.value = "";
 })
+
+function factorial(old_number) {
+    if (old_number == 0 || old_number == 1) {
+        return 1;
+    }
+    
+    else {
+        for (let i = old_number - 1; i >= 1; i--) {
+            old_number = old_number * i;
+        }
+        return old_number;
+    }
+}
